0b669e9c05db251841fe5bc18fd146b03a05972a
thanhpddev <thanhpddev@gmail.com>
2023-08-02 18:16:55 +0700

    29. #20. Axios Retry - Handle Refresh Token

--- src/utils/axios-customize.jsx
+++ src/utils/axios-customize.jsx
@@ -11,6 +11,12 @@ instance.defaults.headers.common = {
   Authorization: `Bearer ${localStorage.getItem("access_token")}`,
 };
 
+const handleRefreshToken = async () => {
+  const res = await instance.get("/api/v1/auth/refresh");
+  if (res && res.data) return res.data.access_token;
+  else null;
+};
+
 // Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
@@ -23,6 +29,8 @@ instance.interceptors.request.use(
   }
 );
 
+const NO_RETRY_HEADER = "x-no-retry";
+
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
@@ -30,9 +38,33 @@ instance.interceptors.response.use(
     // Do something with response data
     return response && response.data ? response.data : response;
   },
-  function (error) {
+  async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      const access_token = await handleRefreshToken();
+      error.config.headers[NO_RETRY_HEADER] = "true";
+      if (access_token) {
+        error.config.headers["Authorization"] = `Bearer ${access_token}`;
+        localStorage.setItem("access_token", access_token);
+        return instance.request(error.config);
+      }
+    }
+
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 400 &&
+      error.config.url === "/api/v1/auth/refresh"
+    ) {
+      window.location.href = "/login";
+    }
+
     return error?.response?.data ?? Promise.reject(error);
   }
 );
